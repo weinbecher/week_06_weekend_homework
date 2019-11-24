b592f763795af6f118e75cb733d44d46033b6d7a
Sophia <382316736@qq.com>
2019-11-24 19:36:26 +0000

    done extension

--- js/app.js
+++ js/app.js
@@ -29,43 +29,57 @@ const createMilkshakeListItem = function(form){
  const milkshakeListItem = document.createElement('li')
  milkshakeListItem.classList.add('milkshake-list-item');
 
+
+
+
+ //mood
+ const moodChoice = document.createElement('p');
+  // console.dir(moodChoice);
+  moodChoice.textContent = form.mood.value;
+
+//name
  const name = document.createElement('h2');
  name.textContent = `${form.name.value} Milkshake`;
- milkshakeListItem.appendChild(name);
 
- const base = document.createElement('h4');
+
+//milkshake name
+ const milkshakeName = document.createElement('h2')
+ milkshakeName.textContent = ` ${moodChoice.textContent} ${name.textContent}`;
+ milkshakeListItem.appendChild(milkshakeName);
+
+
+//base
+ const base = document.createElement('p');
  base.textContent = `${form.base.value} Base`;
  milkshakeListItem.appendChild(base);
 
+//icecream
  const icecream =
- document.createElement('h4');
+ document.createElement('p');
  icecream.textContent = `With ${form.icecream.value} Icecream`;
  milkshakeListItem.appendChild(icecream);
 
- const toppings = document.createElement('h4');
+//toppings
+ const toppings = document.createElement('p');
  toppings.textContent = `And ${form.toppings.value} toppings`;
  form.toppings.value;
  milkshakeListItem.appendChild(toppings);
 
- const straw = document.createElement('h4');
-
+//straw
+ const straw = document.createElement('p');
  const colour = document.createElement('span');
  colour.textContent = '\xa0' ;
  colour.style.backgroundColor = form.straw.value;
  milkshakeListItem.appendChild(colour);
-
  // console.dir(straw);
  straw.textContent = `Straw` ;
 
  let displayStraw = document.createElement('span');
- displayStraw.textContent = ` ${straw.textContent} \xa0 ${colour.textContent}`
-
- // straw.style.backgroundColor = form.straw.value;
+ displayStraw.textContent = ` ${straw.textContent} ${colour.textContent}`
  milkshakeListItem.appendChild(displayStraw);
 
 
  return milkshakeListItem;
-
 }
 
 
